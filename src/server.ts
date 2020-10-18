a9a207d7b8a1a343f65b7b6412ead981e2c70004
Danilo Yorinori <daniloksy@gmail.com>
2020-10-18 00:55:15 -0300

    Tratamento de erros

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import path from 'path';
+import 'express-async-errors';
 
 // Importar arquivo com a definição de conexão com o banco
 import './database/connection';
 
 import routes from './routes';
+import errorHandler from './errors/handler';
 
 const app = express();
 
@@ -12,5 +14,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
 app.listen(3333);
